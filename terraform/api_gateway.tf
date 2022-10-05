f0d449b78076fe355210fd0819f204fd74351469
Cullan Carey <cullancarey@Not-your-Mac-mini.local>
2022-10-05 14:51:17 -0400

    changed lambda policy to use website variable

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -5,7 +5,7 @@
 resource "aws_apigatewayv2_api" "form_intake_api" {
   name          = "contact_form_intake_api"
   protocol_type = "HTTP"
-  description   = "API gateway resource for intake of the contact for on cullancarey.com"
+  description   = "API gateway resource for intake of the contact for on ${var.root_domain_name}"
 
 }
 
