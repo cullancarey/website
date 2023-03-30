e3d5b35cc1228c8b6d6a259d563c203eb5dbbb15
Cullan Carey <cullancarey@MacBook-Pro-2.local>
2023-03-30 11:29:51 -0400

    updating workflows

--- assume_role.sh
+++
@@ -1,4 +0,0 @@
-CREDS=$(aws sts assume-role --role-arn arn:aws:iam::${{ vars.ACCOUNT_ID }}:role/TerraformDeploymentRole-${{ vars.REGION }}-${{ vars.ACCOUNT_ID }} --role-session-name terraform-deployment-${{ vars.REGION }}-${{ vars.ACCOUNT_ID }})
-echo "AWS_ACCESS_KEY_ID=$(echo $CREDS | jq -r '.["Credentials"] | .["AccessKeyId"]')" >> $GITHUB_ENV
-echo "AWS_SECRET_ACCESS_KEY=$(echo $CREDS | jq -r '.["Credentials"] | .["SecretAccessKey"]')" >> $GITHUB_ENV
-echo "AWS_SESSION_TOKEN=$(echo $CREDS | jq -r '.["Credentials"] | .["SessionToken"]')" >> $GITHUB_ENV
