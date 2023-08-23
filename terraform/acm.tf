0714a4664bf6aacba4f08a5c97c20388226676e5
Cullan Carey <cullancarey@MacBook-Pro-3.local>
2023-08-23 14:02:28 -0400

    updating api gateway to use cloudfront

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -35,6 +35,7 @@ resource "aws_acm_certificate_validation" "my_certificate_validation" {
 
 resource "aws_acm_certificate" "intake_api_certificate" {
   domain_name       = var.intake_api_domain
+  provider          = aws.cloudfront-certificate
   validation_method = "DNS"
   options {
     certificate_transparency_logging_preference = "ENABLED"
@@ -52,4 +53,5 @@ resource "aws_acm_certificate" "intake_api_certificate" {
 resource "aws_acm_certificate_validation" "intake_api_certificate_validation" {
   certificate_arn         = aws_acm_certificate.intake_api_certificate.arn
   validation_record_fqdns = [for record in aws_route53_record.intake_api_acm_val_records : record.fqdn]
+  provider                = aws.cloudfront-certificate
 }
--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -98,3 +98,73 @@ data "aws_cloudfront_cache_policy" "cache_policy" {
 data "aws_cloudfront_response_headers_policy" "response_cache_policy" {
   name = "Managed-SecurityHeadersPolicy"
 }
+
+
+
+
+########################################################################################
+########################################################################################################################################################################################################################################################################################################################################################################################################################################################
+
+
+
+
+
+
+resource "aws_cloudfront_distribution" "contact_form_intake_distribution" {
+  origin {
+    domain_name = aws_apigatewayv2_domain_name.intake_api_domain.id
+    origin_id   = var.intake_api_domain
+  }
+
+
+
+
+  aliases             = [var.intake_api_domain]
+  enabled             = true
+  comment             = "Distribution for ${var.intake_api_domain}"
+  price_class         = "PriceClass_100"
+  wait_for_deployment = true
+  tags = {
+    Name = "contact_form_intake_distribution"
+  }
+
+  default_cache_behavior {
+    viewer_protocol_policy     = "redirect-to-https"
+    compress                   = true
+    allowed_methods            = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
+    cached_methods             = ["GET", "HEAD"]
+    target_origin_id           = var.intake_api_domain
+    cache_policy_id            = data.aws_cloudfront_cache_policy.contact_intake_form_cache_policy.id
+    smooth_streaming           = false
+    response_headers_policy_id = data.aws_cloudfront_response_headers_policy.contact_intake_form_response_cache_policy.id
+    origin_request_policy_id   = data.aws_cloudfront_origin_request_policy.contact_intake_form_origin_request_policy.id
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "blacklist"
+      locations        = ["RU"]
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn            = aws_acm_certificate.intake_api_certificate.arn
+    ssl_support_method             = "sni-only"
+    cloudfront_default_certificate = false
+    minimum_protocol_version       = "TLSv1.2_2021"
+  }
+}
+
+
+data "aws_cloudfront_cache_policy" "contact_intake_form_cache_policy" {
+  name = "Managed-CachingDisabled"
+}
+
+
+data "aws_cloudfront_response_headers_policy" "contact_intake_form_response_cache_policy" {
+  name = "Managed-SecurityHeadersPolicy"
+}
+
+data "aws_cloudfront_origin_request_policy" "contact_intake_form_origin_request_policy" {
+  name = "Managed-AllViewer"
+}
